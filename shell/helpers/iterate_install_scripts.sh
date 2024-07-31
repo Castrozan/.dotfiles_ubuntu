23ba7d229f0590f0f0ad8e5a445ba6322e97e47a
castrozan <castro.lucas290@gmail.com>
2024-07-31 18:38:04 -0300

    ci: aaa

--- shell/helpers/iterate_install_scripts.sh
+++ shell/helpers/iterate_install_scripts.sh
@@ -2,13 +2,24 @@
 
 # Function to iterate over install scripts
 # $1: directory with install scripts
+# iterate_install_scripts() {
+#     for file in "$1"; do
+#         echo $1
+#         filename=$(basename "$file")
+#         if ask "Do you want to install ${filename}?"; then
+#             if [ -f "$file" ]; then
+#                 . "$file" # source install script
+#             fi
+#         fi
+#     done
+# }
+
 iterate_install_scripts() {
-    for file in "$1"; do
-        echo $1
-        filename=$(basename "$file")
-        if ask "Do you want to install ${filename}?"; then
-            if [ -f "$file" ]; then
-                . "$file" # source install script
+    for file in "$1"/*; do
+        if [ -f "$file" ]; then  # Only consider files
+            filename=$(basename "$file")
+            if ask "Do you want to install ${filename}?"; then
+                . "$file" # Source the install script
             fi
         fi
     done
