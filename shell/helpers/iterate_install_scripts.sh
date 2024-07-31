5cc81ff58e4c06b21884b5d3712a68a23d8266dc
castrozan <castro.lucas290@gmail.com>
2024-07-31 18:51:04 -0300

    ci: test

--- shell/helpers/iterate_install_scripts.sh
+++ shell/helpers/iterate_install_scripts.sh
@@ -2,6 +2,10 @@
 
 # Function to iterate over install scripts
 iterate_install_scripts() {
+    echo $DOTFILES_HOME
+    echo $INSTALL_SCRIPT_DIR
+    echo "/pkgs/*"
+    echo $(echo "$DOTFILES_HOME$INSTALL_SCRIPT_DIR/pkgs/*")
     for file in $(echo "$DOTFILES_HOME$INSTALL_SCRIPT_DIR/pkgs/*"); do
         filename=$(basename "$file")
         if ask "Do you want to install ${filename}?"; then
