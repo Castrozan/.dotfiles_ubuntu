114a927797618cffb631fcdfe06e736e4fb684a6
castrozan <castro.lucas290@gmail.com>
2024-07-31 17:55:36 -0300

    fix: home

--- shell/helpers/global_vars.sh
+++ shell/helpers/global_vars.sh
@@ -7,11 +7,14 @@ INSTALL_SCRIPT_DIR="/shell"
 # Define the default package manager
 PKG_MGR="apt"
 
+# Define the original home directory
+ORIGINAL_HOME=$(getent passwd $(logname) | cut -d: -f6)
+
 # Default shell location
-SH="${HOME}/.bashrc"
+SH=$ORIGINAL_HOME"/.bashrc"
 
 # Dotfiles home directory
-DOTFILES_HOME="$HOME/.dotfiles_ubuntu"
+DOTFILES_HOME=$ORIGINAL_HOME"/.dotfiles_ubuntu"
 
 # Stow clause
 STOW_CLAUSE="stow --adopt ."
