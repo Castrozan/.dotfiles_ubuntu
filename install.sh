33fff9887e1fe1bfbb14078d4486bc953f73aeb8
castrozan <castro.lucas290@gmail.com>
2024-07-31 18:21:08 -0300

    ci: test

--- install.sh
+++ install.sh
@@ -37,6 +37,7 @@ echo ""
 
 # Ask if pkgs should be installed
 if ask "Do you want to install pkgs?"; then
+    echo "$DOTFILES_HOME$INSTALL_SCRIPT_DIR/pkgs/*"
     iterate_install_scripts "$DOTFILES_HOME$INSTALL_SCRIPT_DIR/pkgs/*"
 fi
 echo ""
