b28dd1c373158a33e40721499afdc401b5a7ca84
castrozan <castro.lucas290@gmail.com>
2024-07-31 19:35:09 -0300

    ci: desisto, bora rodar dentro do container

--- shell/helpers/iterate_install_scripts.sh
+++ shell/helpers/iterate_install_scripts.sh
@@ -4,31 +4,10 @@
 iterate_install_scripts() {
     local dir=$INSTALL_SCRIPTS_DIR
 
-    echo "dir: $dir"
-
-    # Check if the directory exists
-    if [ ! -d "$dir" ]; then
-        echo "Directory does not exist: $dir"
-        return 1
-    fi
-
-    # Use a fallback method to handle file expansion
-    files=$(ls "$dir"/*.sh 2>/dev/null)
-
-    # Check if files variable is empty
-    if [ -z "$files" ]; then
-        echo "No .sh files found in $dir"
-        return 1
-    fi
-
-    for file in $files; do
-        echo "file: $file"
-
+    for file in "$dir"/*.sh; do
         if [ -f "$file" ]; then
-            echo "file is a file"
             local filename=$(basename "$file")
             if ask "Do you want to install ${filename}?"; then
-                echo source install script
                 . "$file" # source install script
             fi
         fi
