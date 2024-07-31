2ab49988754896e96aaa05ac5a89beb98d851b6c
castrozan <castro.lucas290@gmail.com>
2024-07-31 18:29:11 -0300

    fix: expansion of $1

--- shell/helpers/iterate_install_scripts.sh
+++ shell/helpers/iterate_install_scripts.sh
@@ -3,7 +3,7 @@
 # Function to iterate over install scripts
 # $1: directory with install scripts
 iterate_install_scripts() {
-    for file in $1; do
+    for file in "$1"; do
         filename=$(basename "$file")
         if ask "Do you want to install ${filename}?"; then
             if [ -f "$file" ]; then
