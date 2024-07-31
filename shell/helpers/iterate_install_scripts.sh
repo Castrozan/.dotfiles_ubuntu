3ddd3ce421abb38f32d7562320945af2b51724d4
castrozan <castro.lucas290@gmail.com>
2024-07-31 19:24:00 -0300

    s

--- shell/helpers/iterate_install_scripts.sh
+++ shell/helpers/iterate_install_scripts.sh
@@ -4,10 +4,15 @@
 iterate_install_scripts() {
     local dir=$INSTALL_SCRIPTS_DIR
 
+    echo "dir: $dir"
     for file in "$dir"/*.sh; do
+        echo file: $file
+
         if [ -f "$file" ]; then
+            echo "file is a file"
             local filename=$(basename "$file")
             if ask "Do you want to install ${filename}?"; then
+                echo source install script
                 . "$file" # source install script
             fi
         fi
