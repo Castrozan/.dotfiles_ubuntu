bcb54be09764e521f86622bd4668078f3d713a7a
castrozan <castro.lucas290@gmail.com>
2024-07-31 15:06:50 -0300

    feat: comment on top of sourcered files

--- shell/helpers/iterate_config_scripts.sh
+++ shell/helpers/iterate_config_scripts.sh
@@ -8,6 +8,7 @@ iterate_config_scripts() {
                 echo "${YELLOW}$file is already sourced in $SH.${RESET}"
             else
                 # Add the source command to .bashrc
+                echo "# Files sourcered by dotfiles at $DOTFILES_HOME" >> "$SH"
                 echo ". $file" >> "$SH"
                 echo "${GREEN}$file has been sourced.${RESET}"
             fi
