d472f08bbac4d19970376eebcaa86ad66eb4ea20
castrozan <castro.lucas290@gmail.com>
2024-08-01 11:55:56 -0300

    ci: test homebrew

--- shell/helpers/use_brew.sh
+++ shell/helpers/use_brew.sh
@@ -6,7 +6,7 @@ use_brew() {
     # Ask if brew should be installed
     if ask "Do you want to install Homebrew?"; then
         # Check if brew is installed
-        if command -v brew &> /dev/null; then
+        if brew --version; then
             echo "${YELLOW}Homebrew is already installed.${RESET}"
         else
             /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
