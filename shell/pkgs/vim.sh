10f453d02184a9f6a9dae47bc1e3c937dce3475e
castrozan <castro.lucas290@gmail.com>
2024-08-09 17:25:21 -0300

    fix: install tzdata vim dependency noninteractivelly

--- shell/pkgs/vim.sh
+++ shell/pkgs/vim.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
+# tzdata is a vim dependency
+# tzdata is a package that allows you to set the timezone of the system
+# without user interaction. This is useful when you are installing
+# packages that depend on the timezone being set.
+install_tzdata() {
+    export DEBIAN_FRONTEND=noninteractive
+
+    ln -fs /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime
+    apt-get install -y tzdata
+    dpkg-reconfigure --frontend noninteractive tzdata
+}
+
+install_tzdata
 should_install vim
