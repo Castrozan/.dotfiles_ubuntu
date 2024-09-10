6f7187f8640e77b2748b6c50a916a95bffeb144c
castrozan <castro.lucas290@gmail.com>
2024-09-10 20:40:09 -0300

    lint: fzf history

--- shell/configs/fzf_bash_history.sh
+++ shell/configs/fzf_bash_history.sh
@@ -5,22 +5,22 @@
 # Inspired from https://github.com/junegunn/fzf/wiki/examples#searching-file-contents
 
 bind '"\C-f": "\C-x1\e^\er"'
-bind -x '"\C-x1": __fzf_history';
+bind -x '"\C-x1": __fzf_history'
 
-__fzf_history () {
-    __ehc $(
-        history | 
-        fzf --tac --tiebreak=index --height 40% --layout=reverse --border | 
-        perl -ne 'm/^\s*([0-9]+)/ and print "!$1"'
-    )
+__fzf_history() {
+    __ehc "$(
+        history |
+            fzf --tac --tiebreak=index --height 40% --layout=reverse --border |
+            perl -ne 'm/^\s*([0-9]+)/ and print "!$1"'
+    )"
 }
 
 __ehc() {
-    if [ -n "$1" ] ; then
+    if [ -n "$1" ]; then
         bind '"\er": redraw-current-line'
         bind '"\e^": magic-space'
         READLINE_LINE=${READLINE_LINE:+${READLINE_LINE:0:READLINE_POINT}}${1}${READLINE_LINE:+${READLINE_LINE:READLINE_POINT}}
-        READLINE_POINT=$(( READLINE_POINT + ${#1} ))
+        READLINE_POINT=$((READLINE_POINT + ${#1}))
     else
         bind '"\er":'
         bind '"\e^":'
