6d41ae29cf9c4e4af87312e29f0be4e8be669dfc
castrozan <castro.lucas290@gmail.com>
2024-09-10 20:33:59 -0300

    fix: nix install

--- shell/pkgs/nix.sh
+++ shell/pkgs/nix.sh
@@ -4,15 +4,5 @@
 if command -v nix >/dev/null 2>&1; then
     print "Nix already installed" "$YELLOW"
 else
-    # Create a temporary file
-    tmp="$(mktemp)"
-
-    # Download the Nix install script and store it in the temporary file
-    curl -L https://nixos.org/nix/install >"$tmp"
-
-    # Execute the script stored in the temporary file
-    sh "$tmp" --daemon
-
-    # Clean up the temporary file
-    rm "$tmp"
+    curl -L https://nixos.org/nix/install | yes | sh /dev/stdin
 fi
