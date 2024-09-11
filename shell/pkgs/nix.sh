c6b1777a25b5dfc0bd74391ec5c1dc92c2a0ddb0
castrozan <castro.lucas290@gmail.com>
2024-09-10 22:10:27 -0300

    fix: uncomment nix install

--- shell/pkgs/nix.sh
+++ shell/pkgs/nix.sh
@@ -4,6 +4,5 @@
 if command -v nix >/dev/null 2>&1; then
     print "Nix already installed" "$YELLOW"
 else
-    print "TODO: fix nix install script" "$RED"
-    print "curl -L https://nixos.org/nix/install | sh /dev/stdin" "$YELLOW"
+    curl -L https://nixos.org/nix/install | sh /dev/stdin
 fi
