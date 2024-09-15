0c21244b355ad1b6c0e7d8da37f9a89f934cb196
castrozan <castro.lucas290@gmail.com>
2024-09-15 15:53:36 -0300

    fix: ive added the keybinds to the bashrc

--- shell/pkgs/fzf.sh
+++ shell/pkgs/fzf.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
 
 should_install fzf brew
-
-# Check if fzf is installed to install keybinds
-if is_installed fzf brew; then
-    print "Keybinds already installed" $YELLOW
-else
-    # Install useful key bindings and fuzzy completion piping yes to the install script and without showing output on the terminal
-    yes | $(brew --prefix)/opt/fzf/install
-fi
