fd219a3b7c0fed4ec159b172ee3b4c3e8ad3e4b5
castrozan <castro.lucas290@gmail.com>
2024-09-15 16:05:03 -0300

    fix: fzf test and doc the install script

--- shell/pkgs/fzf.sh
+++ shell/pkgs/fzf.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 should_install fzf brew
+print "TODO: fix fzf install script. it works but returns error." "$RED"
--- shell/test/fzf_test.sh
+++ shell/test/fzf_test.sh
@@ -3,27 +3,13 @@
 # Test if fzf is installed
 fzf_test() {
 
-    print "TODO: fix test if fzf is installed." $RED
-    # if brew info fzf &> /dev/null; then
-    #     print "Fzf is installed." $GREEN
-    # else
-    #     print "Fzf is not installed." $RED
-    #     exit 1
-    # fi
-}
-
-# Test if fzf key bindings are working
-fzf_keybinds_test() {
-
-    print "TODO: fix test if fzf key bindings are working." $RED
-    # if ! is_sourced "[ -f ~/.fzf.bash ] && source ~/.fzf.bash" ; then
-    #     print "Fzf key bindings are not working." $RED
-    #     exit 1
-    # else
-    #     print "Fzf key bindings are working." $GREEN
-    # fi
+    if fzf --version &>/dev/null; then
+        print "Fzf is installed." "$GREEN"
+    else
+        print "Fzf is not installed." "$RED"
+        exit 1
+    fi
 }
 
 # Run the test
 fzf_test
-fzf_keybinds_test
