5d8ddfe2ba4c3f8ecb1d2e44453fef5a93dfbd58
castrozan <castro.lucas290@gmail.com>
2024-09-30 09:52:09 -0300

    fix: nvm is installed validation

--- shell/pkgs/nvm.sh
+++ shell/pkgs/nvm.sh
@@ -3,7 +3,7 @@
 . "shell/src/install_with_temp_custom_script.sh"
 
 # Check if nvm is installed
-if nvm --version >/dev/null 2>&1; then
+if [ -d "$HOME/.nvm" ]; then
     print "Nvm already installed" "$YELLOW"
 else
     install_with_temp_custom_script "https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh" "curl" "-L" "bash"
