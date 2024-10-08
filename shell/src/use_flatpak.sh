92d0af4e71427b886a41afa968e21208906e36db
castrozan <castro.lucas290@gmail.com>
2024-10-08 12:28:18 -0300

    fix: add flathub

--- shell/src/use_flatpak.sh
+++ shell/src/use_flatpak.sh
@@ -5,4 +5,8 @@
 # Function to install flatpack
 use_flatpak() {
     should_install flatpak
+
+    if flatpak --version >/dev/null 2>&1; then
+        flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+    fi
 }
