22d286e84e3b6ef18d6023632728c83d7096848e
castrozan <castro.lucas290@gmail.com>
2024-10-08 16:01:46 -0300

    fix: check desktop env

--- shell/src/is_desktop_enviroment.sh
+++ shell/src/is_desktop_enviroment.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-# Function to check if the desktop enviroment is installed
+# Function to check if the desktop enviroment is enabled
 is_desktop_enviroment() {
-    if echo "$DESKTOP_SESSION"; then
+    if [ -n "$DESKTOP_SESSION" ]; then
         return 0
     fi
-
     return 1
 }
